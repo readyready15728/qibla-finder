2ff6b383eda10ba736efe519dd42d276cc9fcb77
Lynn Bradshaw <readyready15728@gmail.com>
2017-10-07 21:06:19 -0400

    Moving contents of app.js to initMap.js because app.js will require Google Maps to be loaded already

--- initMap.js
+++ initMap.js
@@ -1,8 +1,9 @@
 var map;
 
-function initMap() {
+var initMap = function () {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0},
-    zoom: 3
+    zoom: 2
   });
-}
+};
+
